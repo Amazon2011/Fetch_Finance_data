4ca8972b4707cd2c7ea497b944659907dceeb225
Amazon2011 <yongtao.chen@capstonegaming.com>
2017-09-30 15:57:51 +0800

    ddddd

--- FinanceDataFetcher.py
+++ FinanceDataFetcher.py
@@ -78,10 +78,7 @@ class GetStockDataUtils:
         
     #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     #for test
-    #dddddddddddddddd
     #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    
-    
     #dddddd
 	
 	@staticmethod
